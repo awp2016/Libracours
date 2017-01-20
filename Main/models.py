fca61068ac5d2847508bd9d8e98ee1bcd9ffbec8
Alexandra Mereu <mereu.alexandra95@gmail.com>
2017-01-20 12:59:39 +0200

    Add models for Profesor, Subject, ProfesorSunject, Post

--- Main/models.py
+++ Main/models.py
@@ -17,6 +17,11 @@ class UserProfile(models.Model):
 class Domain(models.Model):
     name = models.CharField(max_length=64)
 
+class Student(models.Model):
+    user_id = models.OneToOneField(UserProfile, primary_key=True,
+                                    on_delete=models.CASCADE)
+    score = models.IntegerField()
+
 class StudentGroup(models.Model):
     pass #TO DO
 
@@ -24,7 +29,25 @@ class Group(models.Model):
     leader = models.OneToOneField(StudentGroup, null=True, on_delete=models.SET_NULL)
     year = models.IntegerField()
     domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
-	
+
 class Question(models.Model):
 	text = models.CharField(max_length=256)
 
+class Profesor(models.Model):
+    user_id = models.OneToOneField(UserProfile, primary_key=True,
+                                    on_delete=models.CASCADE)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=64)
+    type = models.CharField(max_length=64)
+
+class ProfesorStudent(models.Model):
+    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL)
+
+class Post(models.Model):
+    author = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
+    prof_subject = models.ForeignKey(ProfesorStudent, null=True, on_delete=models.SET_NULL)
+    date = models.DateField()
+    title = models.CharField(max_length=64)
+    description = models.TextField()
