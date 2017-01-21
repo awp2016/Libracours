a11772297104cbb48010bf00462d3526c3b365dc
Sckipper <yo_da_daniel@yahoo.com>
2017-01-21 15:12:38 +0200

    open pdf option

--- Main/urls.py
+++ Main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^home', views.HomeView.as_view(), name='home'),
     url(r'^login', views.LoginView.as_view(), name='login'),
     url(r'^register', views.RegisterView.as_view(), name='register'),
+    url(r'^pdf', views.PdfView.as_view(), name='pdf'),
     url(r'^profile/(?P<pk>\d+)/$', views.UserProfileView.as_view(),
         name='profile_details'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
