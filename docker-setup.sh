f8c9805e54cd206c7e2a5efc9506aae422f9dac6
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-01-21 17:13:55 +0200

    remmove case; we will start the debug server from container

--- docker-setup.sh
+++ docker-setup.sh
@@ -3,17 +3,9 @@
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-case $1 in
-    manage)
-        python manage.py "${@:7}"
-        ;;
-    run)
-        gunicorn Libracours.wsgi:application \
-            --name Libracours \
-            --bind 0.0.0.0:8000 \
-            --workers 3 \
-            --access-logfile - \
-            --error-logfile -
-        ;;
-    *)
-esac
+gunicorn Libracours.wsgi:application \
+    --name Libracours \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --access-logfile - \
+    --error-logfile -
