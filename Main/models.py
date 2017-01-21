de9e55a02f924ff2fbc367843c6a13d32558e163
Cornel Nițu <cornel.nitu@eaudeweb.ro>
2017-01-21 17:59:34 +0200

    __str__ added

--- Main/models.py
+++ Main/models.py
@@ -11,8 +11,7 @@ def get_avatar_upload_path(instance, filename):
 
 
 def get_attachment_upload_path(instance, filename):
-    username = instance.user.username
-    return 'media/{0}/attachments/%Y/%m/%d/{1}'.format(username, filename)
+    return 'media/attachments/%Y/%m/%d/{0}'.format(filename)
 
 
 class UserProfile(models.Model):
@@ -20,12 +19,18 @@ class UserProfile(models.Model):
     birthdate = models.DateField()
     avatar = models.ImageField(upload_to=get_avatar_upload_path)
 
+    def __str__(self):
+        return self.user
+
     def __unicode__(self):
-        return self.user.username
+        return self.user
 
 
 class Domain(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
     def __unicode__(self):
         return self.name
@@ -36,17 +41,29 @@ class Student(models.Model):
                                    on_delete=models.CASCADE, default = None)
     score = models.IntegerField()
 
+    def __str__(self):
+        return self.user.user
+
     def __unicode__(self):
-        return self.user.user.username
+        return self.user.user
 
 
 class Question(models.Model):
     text = models.CharField(max_length=256)
 
+    def __str__(self):
+        return self.text
+
+    def __unicode__(self):
+        return self.text
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=64)
     type = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
     def __unicode__(self):
         return self.name
@@ -58,6 +75,9 @@ class Professor(models.Model):
     subjects = models.ManyToManyField(Subject, through='ProfessorSubject')
 
 
+    def __str__(self):
+        return self.user_id.user.username
+
     def __unicode__(self):
         return self.user_id.user.username
 
@@ -67,6 +87,9 @@ class ProfessorSubject(models.Model):
                                   related_name='professor_subject')
     subject = models.ForeignKey(Subject, null=True, on_delete=models.SET_NULL,
                                 default = None, related_name='subject')
+
+    def __str__(self):
+        return self.professor.user_id.user.username + ' ' + self.subject.name
 
     def __unicode__(self):
         return self.professor.user_id.user.username + ' ' + self.subject.name
@@ -81,23 +104,38 @@ class Group(models.Model):
     students = models.ManyToManyField(Student)
     professor_subjects = models.ManyToManyField(ProfessorSubject)
 
+    def __str__(self):
+        return self.id
+
     def __unicode__(self):
-        return str(self.id)
+        return self.id
 
 
 class Post(models.Model):
+    title = models.CharField(max_length=64)
+    description = HTMLField()
     author = models.ForeignKey(UserProfile, null=True,
                                on_delete=models.SET_NULL)
     prof_subject = models.ForeignKey(ProfessorSubject, null=True,
                                      on_delete=models.SET_NULL)
     date = models.DateField()
-    title = models.CharField(max_length=64)
-    description = HTMLField()
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
 
 
 class Attachment(models.Model):
     path = models.FileField(upload_to=get_attachment_upload_path)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.post
+
+    def __unicode__(self):
+        return self.post
 
 
 class Comment(models.Model):
@@ -105,11 +143,23 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(UserProfile, null=True, on_delete=models.SET_NULL)
 
+    def __str__(self):
+        return self.text
+
+    def __unicode__(self):
+        return self.text
+
 
 class Vote(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     vote_type = models.BooleanField()  # True - upvote, False - downvote
+
+    def __str__(self):
+        return self.comment
+
+    def __unicode__(self):
+        return self.comment
 
 
 class Answer(models.Model):
@@ -117,10 +167,22 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     is_correct = models.BooleanField()
 
+    def __str__(self):
+        return self.text
+
+    def __unicode__(self):
+        return self.text
+
 
 class Quiz(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.author
+
+    def __unicode__(self):
+        return self.author
 
 
 class Attempt(models.Model):
